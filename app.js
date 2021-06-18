c67b41a26d1c860bb5a7f1c0ae1fd952963d0616
sharo <serinuntiusu@yahoo.co.jp>
2021-06-18 23:39:53 +0900

    pub

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mysql = require('mysql');
 const app = express()
 
+app.use(express.static('public'))
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
